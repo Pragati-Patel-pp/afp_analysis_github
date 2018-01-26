62d445328c030697d0555bd0836076a4aa9a1dc2
Rafal Staszewski <rafal.staszewski@ifj.edu.pl>
2018-01-26 18:56:18 +0100

    cout -> ANA_MSG_INFO + some cleaning

--- AfpAnalysis/src/AfpAnalysisExample.cxx
+++ AfpAnalysis/src/AfpAnalysisExample.cxx
@@ -74,9 +74,15 @@ EL::StatusCode AfpAnalysisExample :: execute ()
 
   const xAOD::EventInfo* eventInfo = 0;
   ANA_CHECK(event->retrieve( eventInfo, "EventInfo") );
-  std::cout << "lb: " << eventInfo->lumiBlock() << "\n";
+  ANA_MSG_INFO("");
+  ANA_MSG_INFO("");
+  ANA_MSG_INFO("New event: " << eventInfo->eventNumber());
+  ANA_MSG_INFO("run: " << eventInfo->runNumber() << " lumi block: " << eventInfo->lumiBlock());
 
   m_AfpTool->reco(); // Do AFP reconstruction
+
+  // Hits
+  ANA_MSG_INFO("Event contains " << m_AfpTool->getHits()->size() << " AFP Si hits");
 
   int nh[4][4] = {0};
   int nhs[4] = {0};
@@ -88,34 +94,50 @@ EL::StatusCode AfpAnalysisExample :: execute ()
     nhs[s]++;
   }
 
-  for(int s=0; s<4; s++)
-  for(int l=0; l<4; l++)
-  std::cout << "hits: " 
-    << " " << s 
-    << " " << l
-    << " " << nh[s][l]
-    << "\n";
+  for(int s=0; s<4; s++){
+    ANA_MSG_INFO("Event contains " << nhs[s] << " AFP Si hits in station " << s);
+    for(int l=0; l<4; l++)
+      ANA_MSG_INFO("Event contains " << nh[s][l] << " AFP Si hits in station " << s << " layer " << l);
+  }
 
+  // Clusters
+  ANA_MSG_INFO("Event contains " << m_AfpTool->getClusters()->size() << " AFP Si clusters");
+  
   int nc[4][4] = {0};
   int ncs[4] = {0};
   for(auto c : *(m_AfpTool->getClusters())){
+    ATH_MSG_INFO("\t" << c);
     int s = c->stationID();
     int l = c->layerID();
     nc[s][l]++;
     ncs[s]++;
   }
 
-  int nt[4] = {0};
+  for(int s=0; s<4; s++){
+    ANA_MSG_INFO("Event contains " << ncs[s] << " AFP Si clusters in station " << s);
+    for(int l=0; l<4; l++)
+      ANA_MSG_INFO("Event contains " << nc[s][l] << " AFP Si clusters in station " << s << " layer " << l);
+  }
 
-  for(auto track : *(m_AfpTool->getTracks())){
-    int s = track->stationID();
+  // Tracks
+  ANA_MSG_INFO("Event contains " << m_AfpTool->getTracks()->size() << " AFP tracks");
+  
+  int nt[4] = {0};
+  for(auto t : *(m_AfpTool->getTracks())){
+    int s = t->stationID();
+    ATH_MSG_INFO("\t" << t);
     nt[s]++;
   }
-  
-  if (m_AfpTool->getProtons()->size() != 0)
-    for (auto proton:*(m_AfpTool->getProtons())) {
-      std::cout << proton << std::endl;
-    }
+
+  for(int s=0; s<4; s++)
+    ANA_MSG_INFO("Event contains " << nt[s] << " AFP tracks in station " << s);
+
+  // Protons
+  ANA_MSG_INFO("Event contains " << m_AfpTool->getProtons()->size() << " AFP protons");
+
+  for(auto proton : *(m_AfpTool->getProtons())){
+    ATH_MSG_INFO("\t" << proton);
+  }
 
   m_AfpTool->clean();
 
